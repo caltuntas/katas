c9e421e07d8aa2995c608fdff2540f678364a866
caltuntas <cihat.altuntas@gmail.com>
2021-08-27 08:51:54 +0300

    chore:movlea.asm

---
+++ asm/movlea.asm
@@ -0,0 +1,21 @@
+
+section .data
+    msg: db "Test", 0
+
+section .text
+    global main
+
+main:
+    push rbp
+
+    xor rbx, rbx
+    xor rax, rax
+    xor rcx, rcx
+    mov rdi, msg
+    lea rsi, [msg] 
+    movsx rbx, byte [msg]
+    mov cl, byte [msg]
+    mov al, [msg]
+
+    pop rbp
+    ret
