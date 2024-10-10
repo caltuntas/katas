276f2472772b521be34a10fbd3d94194f8d12999
caltuntas <cihat.altuntas@gmail.com>
2024-10-10 09:58:35 +0300

    c:callfib update fib1.asm with bcjit generated asm

--- c/callfib/fib1.asm
+++ c/callfib/fib1.asm
@@ -1,38 +1,29 @@
-global fib
-
-fib:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 16             ; Reserve space on the stack
-    mov [rbp - 8], rdi      ; Store n in [rbp - 8]
-
-    cmp dword [rbp - 8], 0x0
-    je return_0             ; If n == 0, return 0
-
-    cmp dword [rbp - 8], 0x1
-    je return_1             ; If n == 1, return 1
-
-    mov rdi, [rbp - 8]      ; Load n
-    sub rdi, 1              ; n - 1
-    call fib                ; fib(n - 1)
-    mov [rbp - 16], rax     ; Save result of fib(n - 1)
-
-    mov rdi, [rbp - 8]      ; Load n
-    sub rdi, 2              ; n - 2
-    call fib                ; fib(n - 2)
-
-    add rax, [rbp - 16]     ; fib(n - 1) + fib(n - 2)
-    jmp return              ; Return result
-
-return_0:
-    mov rax, 0              ; Return 0
-    jmp return
-
-return_1:
-    mov rax, 1              ; Return 1
-    jmp return
-
-return:
-    mov rsp, rbp            ; Restore stack pointer
-    pop rbp                 ; Restore base pointer
-    ret
+push rbp
+mov rbp, rsp
+sub rsp, 200
+mov [rbp-8], rdi
+cmp dword [rbp-8],0
+jg _L1
+mov rax, 0
+jmp FUNC_END
+cmp dword [rbp-8],1
+jne _L2
+mov rax, 1
+jmp FUNC_END
+mov rax, [rbp -8]
+sub rax, 1
+mov rdi, rax
+call fib
+mov [rbp-16],rax
+mov rdx, [rbp -8]
+sub rdx, 2
+mov rdi, rdx
+call fib
+mov [rbp-24],rax
+mov rcx, [rbp -16]
+add rcx, [rbp -24]
+mov rax, rcx
+jmp FUNC_END
+mov rsp, rbp
+pop rbp
+ret
